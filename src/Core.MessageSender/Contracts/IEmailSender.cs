36197bb7de0c201e599db769bc5a2625646a1abe
Denisa Novotna <novotnde@hotmail.com>
2020-08-09 19:54:39 +0200

    added comments to IEmailSender

--- src/Core.MessageSender/Contracts/IEmailSender.cs
+++ src/Core.MessageSender/Contracts/IEmailSender.cs
@@ -9,10 +9,26 @@ namespace Core.MessageSender.Contracts
 {
     public interface IEmailSender
     {
+        /// <summary>
+        /// Sends <see cref="EmailMessage"/> asynchronously.
+        /// </summary>
+        /// <param name="email">Message parameters.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation
+        /// with <see cref="SendResponse"/> representing result of message sending.</returns>
         Task<SendResponse> SendAsync(EmailMessage email);
-
+        /// <summary>
+        /// Sends <see cref="EmailMessage"/> asynchronously.
+        /// </summary>
+        /// <param name="email">Message parameters.</param>
+        /// <returns>A <see cref="Task"/> representing the synchronous operation
+        /// with <see cref="SendResponse"/> representing result of message sending.</returns>
         SendResponse Send(EmailMessage email);
-
+        /// <summary>
+        /// Sends <see cref="EmailMessage"/> asynchronously.
+        /// </summary>
+        /// <param name="email">Message parameters.</param>
+        /// <returns>A <see cref="Task"/> representing the synchronous operation for email creating
+        /// with <see cref="SendEmailRequest"/> representing result of message sending.</returns>
         SendEmailRequest CreateEmailMessage(EmailMessage email);
     }
 }
